b13fb5228843863373ab687da9b63a22f5474ecf
source-toad <joshua.r.brookes@gmail.com>
2023-04-19 08:30:56 +0930

    Updated primary plugin code

--- main.ts
+++ main.ts
@@ -1,137 +1,54 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, MarkdownView, Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class CounterPlugin extends Plugin {
+    async onload() {
+        console.log('Loaded Counter plugin');
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+        this.registerMarkdownCodeBlockProcessor('counter', this.processCounter.bind(this));
+    }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    async processCounter(source: string, el: HTMLElement, ctx: MarkdownView) {
+        const counterRegex = /^(\d+)$/;
+        const match = source.trim().match(counterRegex);
+        if (!match) {
+            el.innerText = 'Invalid syntax. Please use "counter: X" where X is a number.';
+            return;
+        }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        const initialValue = parseInt(match[1]);
+        const containerEl = document.createElement('span');
+        containerEl.classList.add('counter-container');
 
-	async onload() {
-		await this.loadSettings();
+        const arrowLeft = document.createElement('span');
+        arrowLeft.innerText = '<';
+        arrowLeft.classList.add('counter-arrow');
+        containerEl.appendChild(arrowLeft);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        const valueEl = document.createElement('span');
+        valueEl.innerText = initialValue.toString();
+        valueEl.classList.add('counter-value');
+        containerEl.appendChild(valueEl);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        const arrowRight = document.createElement('span');
+        arrowRight.innerText = '>';
+        arrowRight.classList.add('counter-arrow');
+        containerEl.appendChild(arrowRight);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        let currentValue = initialValue;
+        valueEl.addEventListener('click', () => {
+            currentValue++;
+            valueEl.innerText = currentValue.toString();
+        });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        arrowLeft.addEventListener('click', () => {
+            currentValue--;
+            valueEl.innerText = currentValue.toString();
+        });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        el.appendChild(containerEl);
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    onunload() {
+        console.log('Unloading Counter plugin');
+    }
 }
