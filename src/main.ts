df3b6a1d959cd3545409d8a21712aec27cb3e2c6
source-toad <joshua.r.brookes@gmail.com>
2023-04-19 12:11:16 +0930

    Its working!

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,58 @@
-import { Plugin } from "obsidian";
-import { CounterRenderer } from './counter';
-import { CounterPluginSettings } from "./settings";
-//import { DEFAULT_SETTINGS, DialoguePluginSettings, DialogueSettingTab } from './settings';
+import { Plugin, MarkdownPostProcessorContext } from "obsidian";
+import "./styles.css";
 
 export default class CounterPlugin extends Plugin {
-  settings: CounterPluginSettings;
-  
-    async onload() {
-    console.log('Loaded Counter Plugin');
+    counterRegex = /counter:\s*(\d+)\s*/;
 
-    this.registerMarkdownCodeBlockProcessor(
-        'counter', 
-        (src, el, ctx) => {
-      new CounterRenderer (src, el, this.settings);
-    });
-  }
+    async onload() {
+        console.log("Counter plugin loaded");
+
+        this.registerMarkdownPostProcessor(
+            (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+                let nodeList = el.querySelectorAll("code");
+                if (!nodeList.length) return;
+
+                nodeList.forEach((node) => {
+                    let match = this.counterRegex.exec(node.innerText);
+                    if (!match) return;
+
+                    let parent = node.parentElement;
+
+                    let counterValue = parseInt(match[1]);
+
+                    let container = createDiv().createDiv({
+                        cls: "counter"
+                    });
+                    container.setAttr("data-counter-value", counterValue.toString());
+
+                    let counterSpan = container.createSpan();
+                    counterSpan.innerText = counterValue.toString();
+
+                    container
+                        .createDiv({ cls: "counter-button" })
+                        .createEl("button", { text: "-", cls: "decrement-button" })
+                        .addEventListener("click", () => {
+                            counterValue--;
+                            container.setAttr("data-counter-value", counterValue.toString());
+                            counterSpan.innerText = counterValue.toString();
+                        });
+
+                    container
+                        .createDiv({ cls: "counter-button" })
+                        .createEl("button", { text: "+", cls: "increment-button" })
+                        .addEventListener("click", () => {
+                            counterValue++;
+                            container.setAttr("data-counter-value", counterValue.toString());
+                            counterSpan.innerText = counterValue.toString();
+                        });
+
+                    parent.replaceChild(container, node);
+                });
+            }
+        );
+    }
 
     onunload() {
-        console.log('Unloading Counter plugin');
+        console.log("Counter plugin unloaded");
     }
 }
