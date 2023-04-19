ff0360024cecc24cc601efae47cbe5432399ebe7
source-toad <joshua.r.brookes@gmail.com>
2023-04-19 09:55:25 +0930

    more attempts

--- src/settings.ts
+++ src/settings.ts
@@ -1,33 +1,16 @@
-export interface CounterPluginSettings extends Record<string, any> {}
+import { App, Setting, PluginSettingTab } from 'obsidian';
+import DialoguePlugin from './main';
 
-export const DEFAULT_SETTINGS: CounterPluginSettings = {};
+export interface DialoguePluginSettings {
+	defaultLeftTitle: string;
+	defaultRightTitle: string;
+	defaultMessageMaxWidth: string;
+	defaultCommentMaxWidth: string;
+}
 
-export class CounterSettingTab extends PluginSettingTab {
-  plugin: CounterPlugin;
-
-  constructor(app: App, plugin: CounterPlugin) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
-
-  display(): void {
-    const { containerEl } = this;
-
-    containerEl.empty();
-
-    containerEl.createEl('h2', { text: 'Counter Plugin Settings' });
-
-    new Setting(containerEl)
-      .setName('Setting Name')
-      .setDesc('Setting description.')
-      .addText((text) =>
-        text
-          .setPlaceholder('Enter text')
-          .setValue('')
-          .onChange(async (value) => {
-            this.plugin.settings.exampleSetting = value;
-            await this.plugin.saveSettings();
-          })
-      );
-  }
+export const DEFAULT_SETTINGS: DialoguePluginSettings = {
+	defaultLeftTitle: '',
+	defaultRightTitle: '',
+	defaultMessageMaxWidth: '60%',
+	defaultCommentMaxWidth: '60%',
 }
